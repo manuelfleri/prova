6d1ca6fa35f644b991f7bb34ec00a6eff7c9d11f
manuelfleri <manuelfleri98@gmail.com>
2022-03-01 17:34:46 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app= express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+    res.send('Ciao Mondo, sono una APP DI PROVA');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Esempio di web app che gira sulla porta :' + app.get('port'));
+});
